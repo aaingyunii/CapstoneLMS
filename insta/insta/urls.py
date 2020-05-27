8ca3ae9e739205ac58c4d848ae1b82dbc2810c68
Seongmo <gu0112@naver.com>
2020-05-27 15:31:56 +0900

    77

--- insta/insta/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('accounts/', include('accounts.urls')),
-    path('bookmark/', include('bookmark.urls')),
-    path('admin/', admin.site.urls),
-    path('', include('photo.urls')), #빈칸이 이쓰면 include로 보내라.
-]
